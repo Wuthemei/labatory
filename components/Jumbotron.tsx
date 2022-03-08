46343b8815a44a366496dcbb24229022b1dc67c6
themeiwu <wxwcqupt@hotmail.com>
2022-03-08 21:13:00 +0100

    Update Jumbotron.tsx

--- components/Jumbotron.tsx
+++ components/Jumbotron.tsx
@@ -28,9 +28,8 @@ const Jumbotron = (): JSX.Element => {
             gutterBottom
             className="jumbotron-Title"
           >
-            在有生的瞬間能遇到你，竟花光所有{" "}
-            <a href="https://music.163.com/#/song?id=65952&autoplay=true&market=baiduhd" style={{ color: "#ff8c00" }}>
-              運氣。
+            在有生的瞬間能遇到你，竟花光所有{"運氣"}。
+            <a href="https://music.163.com/#/song?id=65952&autoplay=true&market=baiduhd" style={{ color: "#ff8c00" }}> 
             </a>
           </Typography>
           <Typography
