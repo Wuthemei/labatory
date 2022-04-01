86e58d7eafa6192b225c0c932589d14eba7b69c1
themeiwu <wxwcqupt@hotmail.com>
2022-04-01 20:43:02 +0200

    Update jumbotron.ts

--- data/jumbotron.ts
+++ data/jumbotron.ts
@@ -14,11 +14,13 @@
  */
 
 const backgroundPictureLib = [
-  "https://i.picsum.photos/id/311/1600/900.jpg?hmac=yUS02Bgwg81GbTpDuG813r871akJWNv-_Oem7a6PtWY",
-  "https://i.picsum.photos/id/83/1600/900.jpg?hmac=R9BQdLAPaGw27suOHRBe6G6xb7m1XzQlbsx7as1N7-s",
-  "https://i.picsum.photos/id/667/1600/900.jpg?hmac=JScRHUs-2c2rX10nkdYL8sWamAcZM0ax0n18X_N4s0Y",
-  "https://i.picsum.photos/id/888/1600/900.jpg?hmac=hw_TgE4fAZhZAjM5W_sTVY3_SpRrZU7vKw42ZtQt6mo",
-  "https://i.picsum.photos/id/649/1600/900.jpg?hmac=MfwiBIOCS7_7zBii3bCvlKJIUO7rxdhOtDPhgvnn8tk",
+  "https://i.picsum.photos/id/842/1600/900.jpg?hmac=12NH3p2_TQiQzhnYkaQJVr3XwNDqng4-nZXbQ_CucP8",
+  "https://i.picsum.photos/id/642/1600/900.jpg?hmac=Zyg1Q-Yz4L0rHReTxMQ7ffngT38Zky-r56siLMa5SMU",
+  "https://i.picsum.photos/id/441/1600/900.jpg?hmac=GDnko0RbbLYfMHY7LbctM6f2hCKKdFeosCzDqMbNlmU",
+  "https://i.picsum.photos/id/261/1600/900.jpg?hmac=mTGFi5tN-U_CMJ46G0OUXOmYlRy-gwuelxx8ZKVjnTc",
+  "https://i.picsum.photos/id/931/1600/900.jpg?hmac=nzxfNzqISW2vrYATbyBer8zQaLvGApXc0HaP6Xu5OIA",
+  "https://i.picsum.photos/id/141/1600/900.jpg?hmac=_mziYng0Iey3xJRVJbOsR42JzvEx-4krniFL4TSJDgY",
+  "https://i.picsum.photos/id/864/1600/900.jpg?hmac=SXvZyArRFJuSlnjAIrXc5VFs-HRG3BI8Rzbj6qVOdjo",
 ];
 
 export const backgroundPicture =
@@ -41,22 +43,18 @@ export const jumbotronButtonSet: {
     color: "primary",
     links: [
       {
-        localeKey: "whatsnew",
+        localeKey: "WHAT'S UP",
         link: "/tech/2021/05/30/Document-Main#Dev",
       },
-      { localeKey: "demo", link: "https://szhshp.org" },
-      {
-        localeKey: "documents",
-        link: "/tech/2021/05/30/Document-Main",
-      },
+      { localeKey: "GITHUB", link: "https://github.com/themeiwu/labatory/" },
     ],
   },
   {
     setName: "github",
     links: [
       {
-        localeKey: "github",
-        link: "https://github.com/szhshp/NextJS-BlogTemplate-ProjectTitan",
+        localeKey: "LEGACY",
+        link: "https://wuthemei.github.io/",
       },
     ],
   },
